da1f67ff681d662f54619f9e51bb19304ab37091
lucien <lucien@ingenie.fr>
2020-08-26 22:55:02 +0200

    add skill animation & socials tooltips informations

--- src/components/ColumnLeft/SocialsNetworks.js
+++ src/components/ColumnLeft/SocialsNetworks.js
@@ -8,16 +8,16 @@ import {ReactComponent as MailIcon} from "./../../images/icons/mail.svg"
 export function SocialsNetworks() {
   return (
     <div className="SocialsNetworks">
-      <a rel="noopener noreferrer" target="_blank" href="https://codepen.io/lulubeerlu">
+      <a rel="noopener noreferrer" target="_blank" href="https://codepen.io/lulubeerlu" data-tooltip="Codepen">
         <CodepenIcon/>
       </a>
-      <a rel="noopener noreferrer" target="_blank" href="https://github.com/alberluc">
+      <a rel="noopener noreferrer" target="_blank" href="https://github.com/alberluc" data-tooltip="Github">
         <GithubIcon/>
       </a>
-      <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/lucien-albert-ba26bb132">
+      <a rel="noopener noreferrer" target="_blank" href="https://fr.linkedin.com/in/lucien-albert-ba26bb132" data-tooltip="LinkedIn">
         <LinkedInIcon/>
       </a>
-      <a rel="noopener noreferrer" target="_blank" href="mailto:lucienalbert.pro@gmail.com">
+      <a rel="noopener noreferrer" target="_blank" href="mailto:lucienalbert.pro@gmail.com" data-tooltip="lucienalbert.pro@gmail.com">
         <MailIcon/>
       </a>
     </div>
--- src/components/ColumnRight/Skills.js
+++ src/components/ColumnRight/Skills.js
@@ -3,40 +3,44 @@ import "./Skills.css"
 import {useQuery} from "@apollo/client"
 import {GET_SKILLS} from "../../documents/SkillsDocument"
 import {Title} from "../common/Title"
+import {useLoadingTransition} from "../../hooks/useLoadingTransition"
+import {CSSTransition} from "react-transition-group"
 
 export function Skills() {
   const {data: {skills} = {}} = useQuery(GET_SKILLS)
+  const startTransition = useLoadingTransition(!!skills)
 
   if (!skills) return null
 
   const favoriteSkills = skills.filter(skill => skill.isFavorite)
   const otherSkills = skills.filter(skill => !skill.isFavorite)
 
+  const buildSkill = (skill, index) => (
+    <CSSTransition
+      in={startTransition}
+      timeout={100}
+      classNames="SkillItem-animation"
+      key={index}
+    >
+      <div className="SkillItem">
+        <img className="SkillItem-image" src={skill.imagePath} alt={skill.name}/>
+        <div className="SkillItem-data">
+          <Title className="SkillItem-name" level={4} color="primary" font="text">{skill.name}</Title>
+          <span className="SkillItem-level" style={{"--level": skill.level}}/>
+        </div>
+      </div>
+    </CSSTransition>
+  )
+
   return (
     <div className="Skills">
       <Title level={4} font={'text'} color={'primary'} className={'Title-4-page'}>Ce que je préfère</Title>
       <div className="Skills-list">
-        {favoriteSkills.map(skill => (
-          <div className="SkillItem">
-            <img className="SkillItem-image" src={skill.imagePath} alt={skill.name}/>
-            <div className="SkillItem-data">
-              <Title className="SkillItem-name" level={4} color="primary" font="text">{skill.name}</Title>
-              <span className="SkillItem-level" style={{"--level": skill.level}}/>
-            </div>
-          </div>
-        ))}
+        {favoriteSkills.map(buildSkill)}
       </div>
       <Title level={4} font={'text'} color={'primary'} className={'Title-4-page'}>Les autres</Title>
       <div className="Skills-list">
-        {otherSkills.map(skill => (
-          <div className="SkillItem">
-            <img className="SkillItem-image" src={skill.imagePath} alt={skill.name}/>
-            <div className="SkillItem-data">
-              <Title className="SkillItem-name" level={4} color="primary" font="text">{skill.name}</Title>
-              <span className="SkillItem-level" style={{"--level": skill.level}}/>
-            </div>
-          </div>
-        ))}
+        {otherSkills.map(buildSkill)}
       </div>
     </div>
   )
