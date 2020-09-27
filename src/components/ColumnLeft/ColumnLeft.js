4edd6f7fcf02aa14785cac5385874ccfd0c6843d
lucien <lucien@ingenie.fr>
2020-09-27 19:09:03 +0200

    add invisible wave for testing

--- src/components/ColumnLeft/ColumnLeft.js
+++ src/components/ColumnLeft/ColumnLeft.js
@@ -12,6 +12,18 @@ export function ColumnLeft() {
       {device !== 'phone' && (
         <Navigation/>
       )}
+      <div className="Wave-container">
+        <svg width="320" height="1440" viewBox="0 0 320 1440" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0)">
+            <path d="M96 1440L112 1392C128 1344 160 1248 192 1152C224 1056 256 960 266.7 864C277 768 267 672 234.7 576C203 480 149 384 117.3 288C85 192 75 96 69.3 48L64 0H320L320 48C320 96 320 192 320 288C320 384 320 480 320 576C320 672 320 768 320 864C320 960 320 1056 320 1152C320 1248 320 1344 320 1392V1440H96Z"/>
+          </g>
+          <defs>
+            <clipPath id="clip0">
+              <rect width="1440" height="320" fill="white" transform="translate(0 1440) rotate(-90)"/>
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
     </div>
   )
 }
--- src/components/ColumnLeft/Navigation.js
+++ src/components/ColumnLeft/Navigation.js
@@ -8,6 +8,8 @@ import {menus} from "../../config/menus"
 import {SocialsNetworks} from "./SocialsNetworks"
 import {DeviceContext} from "../../contexts/deviceContext"
 
+let toggle = true
+
 export function Navigation() {
 
   function incrementIndex(value) {
@@ -19,9 +21,34 @@ export function Navigation() {
     setCurrentIndex(menus.indexOf(currentItem))
   }
 
+  function changeColor() {
+    const root = document.getElementById('root')
+
+    if (toggle) {
+      /*root.style.setProperty('--color-primary', '#600000')
+      root.style.setProperty('--color-primary-light', 'rgb(96 0 0 / 60%)')
+      root.style.setProperty('--color-secondary', '#ffd2d2')
+      root.style.setProperty('--color-secondary-light', 'rgb(255 210 210 / 85%)')
+      root.style.setProperty('--color-tertiary', '#fff3f3')
+      root.style.setProperty('--color-quaternary', '#fff')
+      root.style.setProperty('--light-box-shadow', '2px 2px 10px #ffebeb')*/
+    } else {
+      /*root.style.setProperty('--color-primary', 'rgba(0, 15, 96, 1)')
+      root.style.setProperty('--color-primary-light', 'rgba(0, 15, 96, .6)')
+      root.style.setProperty('--color-secondary', '#D2E4FF')
+      root.style.setProperty('--color-secondary-light', 'rgba(210, 228, 255, 0.85)')
+      root.style.setProperty('--color-tertiary', '#F3FBFF')
+      root.style.setProperty('--color-quaternary', '#FFF')
+      root.style.setProperty('--light-box-shadow', '2px 2px 10px #EBF3FF')*/
+    }
+
+    toggle = !toggle
+  }
+
   function onCurrentIndexChange() {
     const newItem = menus[currentIndex]
     if (newItem && currentIndex !== lastIndex) {
+      changeColor()
       history.push(newItem.to)
     }
     setLastIndex(currentIndex)
